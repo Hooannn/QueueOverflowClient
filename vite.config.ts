1cf741cd8b1b049e212221d411bddb0d25356b8d
hoanthui <hoanthui123@gmail.com>
2023-12-19 20:30:21 +0700

    ci: use Docker

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     host: '127.0.0.1',
-    port: 8000,
+    port: 5173,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 5173,
   },
   plugins: [react()],
   resolve: {
